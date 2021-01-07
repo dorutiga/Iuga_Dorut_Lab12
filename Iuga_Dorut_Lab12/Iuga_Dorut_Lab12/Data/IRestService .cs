f9b9238814d6a92caa6311162b2e1752a3e549fd
DORU? IUGA <iuga.dorut165271@student.ubbcluj.ro>
2021-01-07 12:19:28 +0200

    Add project files.

---
+++ Iuga_Dorut_Lab12/Iuga_Dorut_Lab12/Data/IRestService .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Iuga_Dorut_Lab12.Models;
+namespace Iuga_Dorut_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
